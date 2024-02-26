01173627d6f4faead002705b10a755b184b903b2
Radik <97843156+Rado700@users.noreply.github.com>
2024-02-26 19:10:33 +0300

    new Project add method

--- src/main/java/com/example/socialnetwork/DB/PostDB.java
+++ src/main/java/com/example/socialnetwork/DB/PostDB.java
@@ -58,11 +58,62 @@ public class PostDB {
     }
     public List<String> getLikes(int id){
 
-        return new ArrayList<>();
+        List<String> newName = new ArrayList<>();
+        try {
+            String like = "SELECT name FROM likes " +
+                    "INNER JOIN users on likes.user_id=users.id " +
+                    "INNER JOIN post on likes.post_id=post.id " +
+                    "WHERE post.id =" + id;
+
+            var stmn = connection.createStatement();
+            var sttm = stmn.executeQuery(like);
+
+            while (sttm.next()){
+
+                String name = sttm.getString("name");
+                newName.add(name);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return newName;
     }
 
     public List<Map<String,Object>> getComments(int id){
 
-        return new ArrayList<>();
+        List<Map<String,Object>>commentAdd=new ArrayList<>();
+
+        try {
+
+            String comment = "SELECT * FROM commentarie " +
+                    "INNER JOIN users on commentarie.user_id=users.id " +
+                    "INNER JOIN post on commentarie.post_id=post.id " +
+                    "WHERE post_id = "+id;
+
+            var stmn = connection.createStatement();
+            var sttm = stmn.executeQuery(comment);
+
+            while (sttm.next()){
+
+                Map<String , Object> map = new HashMap<>();
+                int user_id = sttm.getInt("user_id");
+                String name = sttm.getString("name");
+                String text = sttm.getString("text");
+
+
+                map.put("user_id", user_id);
+                map.put("name", name);
+                map.put("text",text);
+
+                commentAdd.add(map);
+            }
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return commentAdd;
     }
 }
